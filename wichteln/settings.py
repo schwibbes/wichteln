0eead0dde882d1fc6bd66b17b62b333c3c2a05f8
schwibbes <schwibbes2@gmail.com>
2018-01-26 00:21:57 +0100

    WIP

--- wichteln/settings.py
+++ wichteln/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+ 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
