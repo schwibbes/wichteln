a005dce0c834337dd6096b5cca634697037a1a3f
schwibbes <schwibbes2@gmail.com>
2018-01-26 00:35:14 +0100

    wip

--- wichteln/settings.py
+++ wichteln/settings.py
@@ -75,12 +75,13 @@ WSGI_APPLICATION = 'wichteln.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
 #heroku
 import dj_database_url
 db_from_env = dj_database_url.config()
