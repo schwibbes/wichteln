e066343b78cbda8bddffaa675097aef68c5bffd3
schwibbes <schwibbes2@gmail.com>
2018-01-26 00:19:02 +0100

    wip

--- wichteln/settings.py
+++ wichteln/settings.py
@@ -115,6 +115,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
